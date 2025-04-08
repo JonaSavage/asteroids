90547a40d30d0ef9d57736f9f8cc022614069925
JonaSavage <jonathan.stickle@gmail.com>
2025-04-07 19:01:08 -0700

    Drew player sprite

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-from constants import *
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+from constants import *
+from player import Player
 import pygame
 
+# WSL filepath: boot-dev_projects/github.com/JonaSavage/asteroids
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS, 0)
 
     print("\nStarting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        
 
+        screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
+
+        # Framerate = 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
